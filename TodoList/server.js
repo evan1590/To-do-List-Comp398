030f95529f1d30d977eed136b5f5b48a4a9adc70
evan1590 <evanc1590@aol.com>
2014-11-23 12:50:48 -0500

    header stuff

--- TodoList/server.js
+++ TodoList/server.js
@@ -37,7 +37,20 @@ var router = express.Router();
 router.use(function(req, res, next) {
 	// do logging
 	console.log('Something is happening.');
-	next(); // make sure we go to the next routes and don't stop here
+		// global middleware
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+	//next(); 
+	// make sure we go to the next routes and don't stop here
 })
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api) 
